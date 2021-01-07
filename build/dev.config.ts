9bedadf22d123e81d936fd5cc2e9e3774875e045
jianguo-h <1060684755@qq.com>
2021-01-07 11:08:04 +0800

    chore: use webpack-dev-server replace express hot module replacement

--- build/dev.config.ts
+++ build/dev.config.ts
@@ -1,35 +1,12 @@
-import webpack, { Configuration, Entry } from 'webpack';
+import webpack, { Configuration } from 'webpack';
 import webpackMerge from 'webpack-merge';
 import webpackBaseConfig from './base.config';
-
-// config hot module
-const hots = [
-  /* 'react-hot-loader/patch',  */ 'webpack-hot-middleware/client?path=/__webpack_hmr&overlay=true&reload=true',
-];
-const baseConfigEntry = webpackBaseConfig.entry;
-const devConfigEntry: Entry = {};
-if (typeof baseConfigEntry === 'object' && !Array.isArray(baseConfigEntry)) {
-  Object.keys(baseConfigEntry).forEach(entryName => {
-    devConfigEntry[entryName] = hots.concat(
-      baseConfigEntry[entryName] as string
-    );
-  });
-}
 
 const devConfig: Configuration = webpackMerge(webpackBaseConfig, {
   devtool: 'eval-cheap-module-source-map',
   mode: 'development',
-  entry: devConfigEntry,
   output: {
     filename: 'static/js/[name].[fullhash:8].js',
-  },
-  stats: {
-    errors: true,
-    errorDetails: true,
-    warnings: true,
-    colors: true,
-    timings: true,
-    all: false,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 });
