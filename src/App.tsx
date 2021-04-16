6035b6e7908f316a6fc00c914770aaaed9e03c98
jianguo-h <1060684755@qq.com>
2021-04-16 11:32:46 +0800

    test: css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './styles/app.module.less';
+
+console.log(styles);
 
 function App() {
-  return <div id='root'>hello world! this is a react project</div>;
+  return (
+    <div id='root' className={styles.root}>
+      hello world! this is a react project
+    </div>
+  );
 }
 
 export default App;
